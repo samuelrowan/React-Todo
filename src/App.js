697fc9eead63c2153cdcada8dbb50f7bba22c3c9
Samuel Rowan <samuel@samuelrowan.com>
2020-02-20 20:15:30 -0600

    I haven't pushed for awile so, here it is. nothing works.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
 
+const todos = [
+  {
+    name: "the thing to do",
+    id: 0
+  }
+]
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor(){
+    super();
+    this.state = {
+
+    }
+  }
   render() {
     return (
       <div>
